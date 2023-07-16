f09d1562033f03ec9215fd6002137e81d8e023a1
pranjaljain2882 <jain.pranjal12345@gmail.com>
2023-07-16 11:38:07 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        
+        <div className="collapse">
+          <ul className="navbar-nav">
+            <a href="/" className="nav-head">AppName</a>
+            <li className="nav-item">
+              <a href="/" className="nav-link">Home</a>
+            </li>
+            <li className="nav-item">
+              <a href="#Vision" className="nav-link">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+      <div id="Vision" className="page-section">
+        <h1>About Us</h1>
+        <div className="content">
+          <p>
+            At AppName We Post Daily updates on internships and job postings!
+            Our vision is to empower the youth of our nation and make them Atmanirbhar (Self-dependent).
+            By providing them with all kinds of opportunities from various sectors, sharing knowledge, and making them competent enough so that they could excel in any field.
+          </p>
+          <p>
+            If you have any query regarding the site, advertisements, and any other issues, please feel free to contact us at abcd@gmail.com.
+          </p>
+        </div>
+      </div>
+      
+      <div id="contact" className="page-section">
+        <h1>Contact Us</h1>
+        <p>This is the Contact Us page content.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
